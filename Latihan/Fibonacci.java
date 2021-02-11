a68373cbefb44c92bbf8b9aba2ae424ee42be6f0
Mochammad Hairullah <66128826+hoyirul-code@users.noreply.github.com>
2021-02-11 11:58:49 +0700

    Add files via upload

---
+++ Latihan/Fibonacci.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Fibonacci {
+  static int rekursif(int n){
+    if(n == 0 || n == 1){
+      return n;
+    } else {
+      return (rekursif(n-1) + rekursif(n-2));
+    }
+  }
+
+  static void println(String data){
+      System.out.println(data);
+  }
+
+  static void print(String data){
+      System.out.print(data);
+  }
+
+  public static void main(String[]args){
+    Scanner scan = new Scanner(System.in);
+    int i, j = 0;
+    
+    print("Masukkan jumlah deret bilangan fibonacci: ");
+    int n = scan.nextInt();
+    
+    print("Hasil bilangan fibonacci: ");
+    for (i = 0; i < n; i++){
+      print(rekursif(j)+", ");
+      j++;
+    }
+    
+    println("");
+  }
+}
